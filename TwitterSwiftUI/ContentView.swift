c68669eb5bd9b42d926ea2b2d2dbee3a9ea503f0
Rafael Torres <39065424+Raffatoo@users.noreply.github.com>
2022-03-22 05:33:25 -0600

    Build a Twitter with SwiftUI
    Initial commit

---
+++ TwitterSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwitterSwiftUI
+//
+//  Created by Cruz Torres on 22/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
